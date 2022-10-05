cb57e2d04492430629312236961a259fc07ac28d
robertwhite22 <robertwhite.dev@gmail.com>
2022-10-05 12:16:09 -0400

    added head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
 export default function Home() {
   return (
     <main id='home-page' className='page-container home'>
+      <Head>
+        <title>Discoverfy</title>
+      </Head>
       <Image
         src={
           'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3269&q=80'
