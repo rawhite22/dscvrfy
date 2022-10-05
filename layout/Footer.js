780abd59f0274b26347eb4dab772da1795aacab1
robertwhite22 <robertwhite.dev@gmail.com>
2022-10-05 14:43:03 -0400

    updated footer links

--- layout/Footer.js
+++ layout/Footer.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Footer() {
   return (
     <footer className='footer'>
-      <Link href={'/'}>
+      <Link href={'https://github.com/rawhite22/dscvrfy'}>
         <a>
           <FontAwesomeIcon className='footer-icon' icon={faGithub} />
         </a>
       </Link>
-      <Link href={'/'}>
+      <Link href={'https://www.instagram.com/_discoverfy/'}>
         <a>
           {' '}
           <FontAwesomeIcon className='footer-icon' icon={faInstagram} />
